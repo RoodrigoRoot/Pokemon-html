b3b96aefa9b4ac6a9218771bb73160a0060b6504
Rodrigo <roodrigoroot@gmail.com>
2019-12-20 12:27:39 -0600

    Images and type added

--- UIPokemons.js
+++ UIPokemons.js
@@ -5,11 +5,20 @@ class UIPokemon{
     async UICreatePokemon(){
         let pokemons_response = await this.pokemon.getPokemons()
         let pokemons_article = document.getElementById("pokemons") 
-        pokemons_response.map( (item)=>{
-        
+        pokemons_response.map( async (item,index)=>{
+            console.log(index)
+        let pokemon_response = await axios.get(item.url)
+        let {data} = pokemon_response
+        let {types} = data
         let div = document.createElement('div') 
         div.className='cards'
-        div.innerHTML=`<h3>${item.name} </h3>`
+        div.innerHTML=`<h3>${String(item.name).toLocaleUpperCase()} </h3>
+        <img src="${data.sprites.front_default}"/>
+        <hr/>
+        <p>Tipo: ${data.types[0].type.name+","} ${data.types[1].type.name} </p>
+        <p>Peso: ${data.height}Kg</p>
+        <a href="#" class="btn">Ver más</a>
+        `
         pokemons_article.appendChild(div)            
       
         } )
